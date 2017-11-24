a0a846a8ad475d4967553846c27d1e976a1b6351
Aukiha <aukihat@gmail.com>
2017-11-24 17:15:12 -0500

    fade in shit

---
+++ js/fadein.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+  $("#simratPic").click(function() {
+    $("#simratProfile").fadeToggle();
+  });
+});
+
+$(document).ready(function() {
+  $("#rupreetPic").click(function() {
+    $("#rupreetProfile").fadeToggle();
+  });
+});
+
+$(document).ready(function() {
+  $("#ameliaPic").click(function() {
+    $("#ameliaProfile").fadeToggle();
+  });
+});
+
+$(document).ready(function() {
+  $("#bobPic").click(function() {
+    $("#bobProfile").fadeToggle();
+  });
+});
+
+$(document).ready(function() {
+  $("#manrosePic").click(function() {
+    $("#manroseProfile").fadeToggle();
+  });
+});
