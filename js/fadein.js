9081f850a57a48c011c9dc2e30d12b5a3656009d
Aukiha <aukihat@gmail.com>
2017-11-27 10:09:23 -0500

    oh shit the beta version works

--- js/fadein.js
+++ js/fadein.js
@@ -27,3 +27,32 @@ $(document).ready(function() {
     $("#manroseProfile").fadeToggle();
   });
 });
+
+var toggle = false;
+$(document).ready(function() {
+  $("#mangatPic").click(function() {
+    if (toggle == false) {
+      $("#amanpreetPic").css("z-index", "-4200")
+      $("#amanpreetPic").css("opacity", "0.6")
+      $("#nishchayPic").css("z-index", "-4200")
+      $("#nishchayPic").css("opacity", "0.6")
+      $("#charithPic").css("z-index", "-4200")
+      $("#charithPic").css("opacity", "0.6")
+      $(".body").css("background-color", "#252530")
+      $("#box1").fadeToggle();
+      $("#box1").animate({left: '100px'});
+      toggle = true;
+    }
+    else {
+      $("#amanpreetPic").css("z-index", "420")
+      $("#amanpreetPic").css("opacity", "1")
+      $("#nishchayPic").css("z-index", "420")
+      $("#nishchayPic").css("opacity", "1")
+      $("#charithPic").css("z-index", "420")
+      $("#charithPic").css("opacity", "1")
+      $(".body").css("background-color", "#272739")
+      $("#box1").fadeToggle();
+      toggle = false;
+    }
+  });
+});
