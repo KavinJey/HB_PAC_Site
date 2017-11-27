dd839c74de3737ef0eb496518b98878a8e221c58
Aukiha <aukihat@gmail.com>
2017-11-27 13:47:30 -0500

    added comments for that SICC documentation and more efficient collaborative work efforts

--- js/fadein.js
+++ js/fadein.js
@@ -1,3 +1,4 @@
+// Test Functions
 $(document).ready(function() {
   $("#simratPic").click(function() {
     $("#simratProfile").fadeToggle();
@@ -28,10 +29,13 @@ $(document).ready(function() {
   });
 });
 
+// Clicking on Mangat's Portrait
 var toggle = false;
 $(document).ready(function() {
   $("#mangatPic").click(function() {
+    // if description hasn't been toggled already
     if (toggle == false) {
+      // dim all other pictures and background
       $("#amanpreetPic").css("z-index", "-4200")
       $("#amanpreetPic").css("opacity", "0.6")
       $("#nishchayPic").css("z-index", "-4200")
@@ -39,11 +43,15 @@ $(document).ready(function() {
       $("#charithPic").css("z-index", "-4200")
       $("#charithPic").css("opacity", "0.6")
       $(".body").css("background-color", "#252530")
+      // fade in box and slide in to the right
       $("#box1").fadeToggle();
       $("#box1").animate({left: '120px'});
+      // toggle variable
       toggle = true;
     }
+    // if the description has been toggled already
     else {
+      // reset the state of all the other pictures and background
       $("#amanpreetPic").css("z-index", "420")
       $("#amanpreetPic").css("opacity", "1")
       $("#nishchayPic").css("z-index", "420")
@@ -51,7 +59,9 @@ $(document).ready(function() {
       $("#charithPic").css("z-index", "420")
       $("#charithPic").css("opacity", "1")
       $(".body").css("background-color", "#272739")
+      // fade out the box
       $("#box1").fadeToggle();
+      // toggle variable
       toggle = false;
     }
   });
